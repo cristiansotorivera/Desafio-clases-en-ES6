099aa7c872ad924a9bf284045f0d38a0903611fb
Cristian Soto <cristiansotorivera@gmail.com>
2024-03-17 13:17:41 -0300

    last

--- assets/js/impuestos.js
+++ assets/js/impuestos.js
@@ -1,31 +1,3 @@
-/* 
-
-export class Impuesto {
-    #montoBrutoAnual
-    #deducciones
-
-    constructor(montoBrutoAnual, deducciones) {
-        this.#montoBrutoAnual = montoBrutoAnual
-        this.#deducciones = deducciones
-    }
-
-    get montoBrutoAnual() {
-        return this.#montoBrutoAnual;
-    }
-    set montoBrutoAnual(nuevoMontoBrutoAnual) {
-        this.#montoBrutoAnual = nuevoMontoBrutoAnual;
-    }
-
-    get deducciones() {
-        return this.#deducciones;
-    }
-    set deducciones(nuevasDeducciones) {
-        this.#deducciones = nuevasDeducciones;
-    }
-}
-
- */
-
 
 
 
